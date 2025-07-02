c862dcbb4771599f9e6ef36699b452108fe953a7
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-07-02 17:59:41 +0300

    backedn tasks are passing

--- labs/lab02/backend/chatcore/chatcore.go
+++ labs/lab02/backend/chatcore/chatcore.go
@@ -43,20 +43,73 @@ func NewBroker(ctx context.Context) *Broker {
 // Run starts the broker event loop (goroutine)
 func (b *Broker) Run() {
 	// TODO: Implement event loop (fan-in/fan-out pattern)
+	for {
+		select {
+		case msg := <-b.input:
+			b.routeMessage(msg)
+		case <-b.ctx.Done():
+			return
+		case <-b.done:
+			return
+		}
+	}
+}
+
+// routeMessage routes a message to appropriate recipients
+func (b *Broker) routeMessage(msg Message) {
+	b.usersMutex.RLock()
+	defer b.usersMutex.RUnlock()
+
+	if msg.Broadcast {
+		// Send to all users (including sender)
+		for _, recvChan := range b.users {
+			select {
+			case recvChan <- msg:
+			default:
+				// Channel is full, skip
+			}
+		}
+	} else {
+		// Send private message
+		if recvChan, exists := b.users[msg.Recipient]; exists {
+			select {
+			case recvChan <- msg:
+			default:
+				// Channel is full, skip
+			}
+		}
+	}
 }
 
 // SendMessage sends a message to the broker
 func (b *Broker) SendMessage(msg Message) error {
-	// TODO: Send message to appropriate channel/queue
-	return nil
+	select {
+	case <-b.ctx.Done():
+		return b.ctx.Err()
+	default:
+	}
+	select {
+	case b.input <- msg:
+		return nil
+	case <-b.ctx.Done():
+		return b.ctx.Err()
+	default:
+		return context.DeadlineExceeded
+	}
 }
 
 // RegisterUser adds a user to the broker
 func (b *Broker) RegisterUser(userID string, recv chan Message) {
 	// TODO: Register user and their receiving channel
+	b.usersMutex.Lock()
+	defer b.usersMutex.Unlock()
+	b.users[userID] = recv
 }
 
 // UnregisterUser removes a user from the broker
 func (b *Broker) UnregisterUser(userID string) {
 	// TODO: Remove user from registry
+	b.usersMutex.Lock()
+	defer b.usersMutex.Unlock()
+	delete(b.users, userID)
 }
